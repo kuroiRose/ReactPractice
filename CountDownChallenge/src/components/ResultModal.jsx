ceab50d18d29f3819216431fd925204a21770da5
Nisa Parveen Shaikh <kuroibarakarose@gmail.com>
2024-10-06 20:47:36 +0530

    Creating project CountDownChallenge

---
+++ CountDownChallenge/src/components/ResultModal.jsx
@@ -0,0 +1,23 @@
+import { forwardRef } from "react";
+import { createPortal } from 'react-dom';
+
+const ResultModal = forwardRef(function ResultModal({targetTime, timeRemaining, onReset}, ref){
+    const userLost = timeRemaining <= 0;
+    const formattedRemainingTime = (timeRemaining/1000).toFixed(2);
+    const score = Math.round((1 - timeRemaining / (targetTime * 1000)) * 100);
+
+    return createPortal(
+        <dialog ref={ref} className="result-modal" onClose={onReset}>
+            {userLost && <h2>You lost</h2>}
+            {!userLost && <h2>Your score: {score}</h2>}
+            <p>The target time was <strong>{targetTime} seconds.</strong></p>
+            <p>You stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong></p>
+            <form method="dialog" onSubmit={onReset}>
+                <button>Close</button>
+            </form>
+        </dialog>,
+        document.getElementById('modal')
+    );
+})
+
+export default ResultModal;
